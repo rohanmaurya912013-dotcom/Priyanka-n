066f43c6231c4c8356db7da9c3fb6b0ad68f20d0
rohanmaurya912013-dotcom <rohanmaurya912013@gmail.com>
2026-07-16 01:00:03 +0530

    Add event listeners for music toggle and enter button

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+document.getElementById('musictoggle').addEventListener('click', function() {
+  alert('Music toggled!');
+});
 
+document.getElementById('enterbtn').addEventListener('click', function() {
+  document.getElementById('welcomescreen').classList.add('hidden');
+  document.getElementById('main-content').classList.remove('hidden');
+});
